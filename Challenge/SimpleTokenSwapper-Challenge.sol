3c38595dc9be708d3768ef23b4c6a91359c20058
kelreel <derodroll@gmail.com>
2024-09-29 22:54:44 +0300

    foundry

--- Challenge/SimpleTokenSwapper-Challenge.sol
+++ Challenge/SimpleTokenSwapper-Challenge.sol
@@ -4,8 +4,15 @@ pragma solidity ^0.8.24;
 // Implement Uniswap swap interface
 // Implement library to help with token transfers
 
+// Import the Uniswap V3 interface
+import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
 contract SimpleTokenSwapper {
     // Define the Uniswap Router address and the WETH address variable
+    address public immutable routerAddress =
+        "0xfc30937f5cDe93Df8d48aCAF7e6f5D8D8A31F636";
 
     // Define the constructor
     constructor() {
